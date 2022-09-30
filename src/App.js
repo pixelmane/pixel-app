4351f384724af68b61bdd479facaaeeac190b7f5
JayHaji <jaymainframe@JayHajis-MacBook-Pro.local>
2022-09-30 14:23:16 -0400

    style answer squares to center

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           <div style={{margin: '0 auto', width: '75%', aspectRatio: '1 / 1'}}>
           <EndGameboard />
           </div>
-          <div style={{display: 'flex', marginTop: '20px', marginBottom: '20px'}}>
+          <div style={{display: 'flex', justifyContent: 'center', marginTop: '20px', marginBottom: '20px'}}>
             {checkArray.map(element => element === '?' ? <div style={{display: 'flex', marginLeft: '3px',justifyContent: 'center', alignItems: 'center', color: 'white', border: '2px solid white', height: '50px', width: '50px'}}>{element}</div> : <div style={{display: 'flex', marginLeft: '3px',justifyContent: 'center', alignItems: 'center', backgroundColor: 'green', color: 'white', border: '2px solid white', height: '50px', width: '50px'}}>{element}</div>)}
             
             </div>
