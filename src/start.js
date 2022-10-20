8d4a3d6f037db2d19cee0d361ee1ee2b327009f8
JayHaji <jaymainframe@JayHajis-MacBook-Pro.local>
2022-10-20 11:38:16 -0400

    styling of start and instruction screen

--- src/start.js
+++ src/start.js
@@ -1,23 +1,54 @@
 import poplogo from './poplogo.png'
 import { Link } from "react-router-dom"
+import sponge from './sponge.png'
 import { collection } from "./collection.js"
 export function Start() {
     let starter = collection.length - 1;
+    const goalArray = ['?','?','?','?','?'];
+    const howArray = ['H','O','W','T','O','P','L','A','Y']
+    const colorSample = ['blue','yellow','red']
     return (
         <div style={{display: 'flex', justifyContent: 'center', width: '80%', flexWrap: 'wrap', margin: '0px auto', padding: '10px', borderRadius: '5px', marginTop: '15px'}}>
             <div style={{width: 'min(400px, 100%)'}}><img alt='poplogo' src={poplogo} style={{height: '100%', width: '100%'}}/></div>3
             <div style={{display: 'flex', justifyContent: 'center', width: '100%'}}><Link to={starter.toString()} ><button className='navButton' >start</button></Link></div>
             <div style={{display: 'block', width: 'min(400px, 100%)'}}>
-                            <div style={{width: 'min(400px, 100%)', border: '2px solid white', borderRadius: '5px', padding: '5px'}}>
-                <h3 style={{color: 'white', margin: '0px'}}>Goal:</h3>
-                <h3 style={{color: 'white', marginLeft: '15px',marginTop: '0', marginBottom: '10px'}}>Correctly guess pixelated image in as few tries as possible.</h3>
+                            <div style={{width: 'min(400px, 100%)', borderRadius: '5px'}}>
+            {/*<div style={{display: 'flex',justifyContent: 'center', fontWeight: 'bold', fontSize: '30px', maxWidth: '400px', margin: '0 auto'}}>
+            {goalArray.map((element, index) => <div style={{ display: 'flex', marginLeft: '3px',justifyContent: 'center', alignItems: 'center', color: 'white', border: '2px solid white',backgroundColor: 'green', width: `${70/goalArray.length}%`, aspectRatio: '1 / 1'}}>{goalArray[index] ? goalArray[index] : '?'}</div>)}
+    </div>*/}
+            <div style={{display: 'flex',justifyContent: 'center', fontWeight: 'bold', fontSize: '16px', maxWidth: '400px', margin: '0 auto'}}>
+            {howArray.map((element, index) => <div style={{ display: 'flex', marginLeft: '3px',justifyContent: 'center', alignItems: 'center', color: 'white', border: '2px solid white',backgroundColor: 'green', width: `${100/howArray.length}%`, aspectRatio: '1 / 1'}}>{howArray[index] ? howArray[index] : '-'}</div>)}
+            
             </div>
+                 </div>
             <div style={{width: 'min(400px, 100%)', border: '2px solid white', borderRadius: '5px', padding: '5px', marginTop: '10px'}}>
-                <h3 style={{color: 'white', margin: '0px'}}>How to play:</h3>
-                <h3 style={{color: 'white',marginTop: '0px', marginLeft: '15px'}}>1. Choose a color square.  Pixels containing chosen color will be revealed.</h3>
-                <h3 style={{color: 'white', marginLeft: '15px'}}>2. After each color reveal, you will have an opportunity to guess the image. Correct letters will appear green.</h3>
-                <h3 style={{color: 'white', marginLeft: '15px'}}>3. Repeat until you guess correctly or give up.</h3>
-                <h4 style={{color: 'white'}}>p.s. - one color is a decoy and will not reveal any pixels...</h4>
+                <div style={{marginTop: '10px', display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center'}}>
+                <div style={{width: '50%', display: 'flex', justifyContent: 'center'}}><h3 style={{color: 'white', margin: '0px'}}>Select Color:</h3></div>
+                <div style={{display: 'flex', width: '50%', justifyContent: 'center', fontWeight: 'bold', fontSize: '16px', margin: '0 auto'}}>
+            {colorSample.map((element, index) => <div style={{ display: 'flex', marginLeft: '3px',justifyContent: 'center', alignItems: 'center', color: 'white', border: '2px solid white',backgroundColor: element, width: `${40/colorSample.length}%`, aspectRatio: '1 / 1'}}></div>)}
+            
+            </div>
+            
+            </div>
+            <div style={{marginTop: '10px',display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center'}}>
+                <div style={{width: '50%', display: 'flex', justifyContent: 'center'}}><h3 style={{color: 'white', margin: '0px'}}>Pixels revealed:</h3></div>
+                <div style={{display: 'flex', width: '50%', justifyContent: 'center', fontWeight: 'bold', fontSize: '16px', margin: '0 auto'}}>
+                <div style={{width: '70%', aspectRatio: '1 / 1', maxWidth: '200px'}}><img alt='sample of gameboard' src={sponge} style={{height: '100%', width: '100%'}}/></div>
+            
+            </div>
+            
+            </div>
+            <div style={{marginTop: '10px',marginBottom: '10px', display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center'}}>
+                <div style={{width: '50%', display: 'flex', justifyContent: 'center'}}><h3 style={{color: 'white', margin: '0px'}}>Guess image:</h3></div>
+                <div style={{display: 'flex', width: '50%', justifyContent: 'center', fontWeight: 'bold', fontSize: '16px', margin: '0 auto'}}>
+                {goalArray.map((element, index) => <div style={{ display: 'flex', marginLeft: '3px',justifyContent: 'center', alignItems: 'center', color: 'white', border: '2px solid white',backgroundColor: 'black', width: `${100/goalArray.length}%`, aspectRatio: '1 / 1'}}>{goalArray[index] ? goalArray[index] : '-'}</div>)}
+            
+            </div>
+            
+            </div>
+            <div style={{width: '100%', textAlign: 'center'}}>
+             <h5 style={{color: 'white', margin: '0 auto'}}>*one color will be a decoy and will not reveal any pixels</h5>
+            </div>
             </div>
                 </div>
         </div>
